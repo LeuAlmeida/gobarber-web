f0c27e3fe4069a89faf35dc4277257da145ebe26
LeuAlmeida <leo@webid.net.br>
2020-02-20 17:16:46 -0300

    feat(signin): improving Login component page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Seu senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
 
 export default SignIn;
