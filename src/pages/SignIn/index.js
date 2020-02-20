13aab14c99b6665966477c3afefd43c95e9845a6
LeuAlmeida <leo@webid.net.br>
2020-02-20 17:28:40 -0300

    feat(SignIn): improving signin form and validations

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
-// import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 function SignIn() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="Gobarber" />
 
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Seu senha secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Seu senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
