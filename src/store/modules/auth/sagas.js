8aede8d9a52a3e831cc4fc0384922a97193d033f
LeuAlmeida <leo@webid.net.br>
2020-03-03 13:43:13 -0300

    feat(auth): implementing token persistor

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,6 +21,8 @@ export function* signIn({ payload }) {
       toast.error('Usuário não é prestador');
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -30,4 +32,37 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    history.push('/');
+    toast.success('Registro efetuado com sucesso, por favor realize o login.');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados.');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
