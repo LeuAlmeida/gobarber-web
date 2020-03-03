dfe0e1b787ba740197d95cb432f825781fcce904
LeuAlmeida <leo@webid.net.br>
2020-03-03 13:44:11 -0300

    feat(Dashboard): create api consume into dashboard file

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 function Dashboard() {
+  api.get('appointments');
+
   return <h1>Dashboard</h1>;
 }
 
