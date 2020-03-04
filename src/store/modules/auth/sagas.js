fbd7972b77c75665c0978eb4343b4be725893414
LeuAlmeida <leo@webid.net.br>
2020-03-04 11:10:41 -0300

    feat(user): create Sign Out saga

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -61,8 +61,14 @@ export function setToken({ payload }) {
   }
 }
 
+export function signOut() {
+  history.push('/');
+  toast.info('Você saiu do GoBarber.');
+}
+
 export default all([
   takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
 ]);
