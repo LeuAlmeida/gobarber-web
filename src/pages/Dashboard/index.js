a4a3bab2ec61f42821409ffb3e7e533149566994
LeuAlmeida <leo@webid.net.br>
2020-03-04 11:22:59 -0300

    feat(Dashboard): create list content

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 // import api from '~/services/api';
 
-import { Container } from './styles';
+import { Container, Time } from './styles';
 
 function Dashboard() {
   return (
@@ -16,6 +16,25 @@ function Dashboard() {
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Léu Almeida</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Léu Almeida</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Léu Almeida</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Léu Almeida</span>
+        </Time>
+      </ul>
     </Container>
   );
 }
